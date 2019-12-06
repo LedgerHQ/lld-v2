0c4bf72e41472931526738853dbcae07b5e01fc4
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2019-12-06 14:22:28 +0100

    husky pre-add hooks

--- internals/start.js
+++ internals/start.js
@@ -1,45 +1,50 @@
 #!/usr/bin/env node
 
-const webpack = require('webpack');
-const ProgressPlugin = require('webpack/lib/ProgressPlugin');
-const Listr = require('listr');
-const { Observable } = require('rxjs');
+const webpack = require('webpack')
+const ProgressPlugin = require('webpack/lib/ProgressPlugin')
+const Listr = require('listr')
+const { Observable } = require('rxjs')
 
 console.log(process.env.INIT_CWD)
 
 const configs = {
   renderer: require('../renderer.webpack.config'),
-  main: require('../main.webpack.config')
+  main: require('../main.webpack.config'),
 }
 
-const runWebpack = config => new Observable(observer => {
-  const compiler = webpack(config)
+const runWebpack = config =>
+  new Observable(observer => {
+    const compiler = webpack(config)
 
-  compiler.apply(new ProgressPlugin((percentage, msg) => {
-    observer.next(`${msg} ${Math.floor(percentage * 100)}%`)
-  }))
+    compiler.apply(
+      new ProgressPlugin((percentage, msg) => {
+        observer.next(`${msg} ${Math.floor(percentage * 100)}%`)
+      }),
+    )
 
-  compiler.run((err, stats) => {
-    if (err) {
-      return observer.error(err)
-    }
-    if (stats.hasErrors()) {
-      return observer.error(stats.toString({ colors: true }))
-    }
-    observer.complete()
+    compiler.run((err, stats) => {
+      if (err) {
+        return observer.error(err)
+      }
+      if (stats.hasErrors()) {
+        return observer.error(stats.toString({ colors: true }))
+      }
+      observer.complete()
+    })
   })
-})
 
-
-const tasks = new Listr([
-  {
-    title: 'Building main bundle',
-    task: () => runWebpack(configs.main)
-  },
-  {
-    title: 'Building renderer bundle',
-    task: () => runWebpack(configs.renderer)
-  }
-], { concurrent: true })
+const tasks = new Listr(
+  [
+    {
+      title: 'Building main bundle',
+      task: () => runWebpack(configs.main),
+    },
+    {
+      title: 'Building renderer bundle',
+      task: () => runWebpack(configs.renderer),
+    },
+  ],
+  { concurrent: true },
+)
 
 tasks.run()
