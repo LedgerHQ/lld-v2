04abfed1bd2573cb3d22f2b729680c5248125d7a
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-01-07 20:29:56 +0100

    add add accounts button

--- src/renderer/screens/accounts/index.js
+++ src/renderer/screens/accounts/index.js
@@ -1,10 +1,16 @@
 // @flow
 import React from "react";
+import { connect } from "react-redux";
+import { openModal } from "~/renderer/actions/modals";
+import Button from "~/renderer/components/Button";
 
-const Accounts = () => (
+const Accounts = ({ dispatch }: { dispatch: (*) => void }) => (
   <>
     <h1>Accounts</h1>
+    <Button onClick={() => dispatch(openModal("MODAL_ADD_ACCOUNTS"))}>Add Accounts</Button>
   </>
 );
 
-export default Accounts;
+const screen: React$ComponentType<{}> = connect()(Accounts);
+
+export default screen;
