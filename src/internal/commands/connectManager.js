47a305cb25af90774005f9daedc6f9a3b2391372
Juan Cortes Ross <juan@bohem.io>
2020-01-08 15:58:02 +0100

    Manager connect + lottie wip

--- src/internal/commands/connectManager.js
+++ src/internal/commands/connectManager.js
@@ -5,55 +5,70 @@ import { withDevice } from "@ledgerhq/live-common/lib/hw/deviceAccess";
 import getDeviceInfo from "@ledgerhq/live-common/lib/hw/getDeviceInfo";
 import { listApps } from "@ledgerhq/live-common/lib/apps/hw";
 import type { DeviceInfo } from "@ledgerhq/live-common/lib/types/manager";
-import { concatMap, scan } from "rxjs/operators";
+import { concatMap, scan, startWith } from "rxjs/operators";
 import { from, of } from "rxjs";
 import type { ListAppsResult } from "@ledgerhq/live-common/lib/apps";
 import logger from "~/logger";
 import manager from "@ledgerhq/live-common/lib/manager";
+import getAppAndVersion from "@ledgerhq/live-common/lib/hw/getAppAndVersion";
 
 type Input = {
   devicePath: string,
 };
 
 export type ManagerConnectState = {
-  deviceInfo?: DeviceInfo,
-  allowManagerRequested: boolean,
   allowManagerGranted: boolean,
+  allowManagerRequested: boolean,
+  deviceInDashboard: boolean,
+  deviceInfo?: DeviceInfo,
+  isLoading: boolean,
   listAppsRes?: ListAppsResult,
 };
 
 const seed: ManagerConnectState = {
-  allowManagerRequested: false,
   allowManagerGranted: false,
+  allowManagerRequested: false,
+  deviceInDashboard: true,
+  isLoading: false,
 };
 
 const cmd = ({ devicePath }: Input): Observable<ManagerConnectState> =>
   withDevice(devicePath)(transport =>
-    from(getDeviceInfo(transport)).pipe(
-      concatMap(deviceInfo => {
-        if (deviceInfo.isBootloader || deviceInfo.isOSU) {
-          return of({ ...seed, deviceInfo });
+    from(getAppAndVersion(transport)).pipe(
+      concatMap(appAndVersion => {
+        if (!["BOLOS", "OLOS\u0000"].includes(appAndVersion.name)) {
+          // Not in dashboard
+          return of({ ...seed, deviceInDashboard: false });
         }
 
-        // Preload things in parallel
-        manager.getLatestFirmwareForDevice(deviceInfo).catch(e => {
-          logger.warn(e);
-        });
-
-        return listApps(transport, deviceInfo).pipe(
-          scan((state: ManagerConnectState, e: *) => {
-            const response = { ...state, deviceInfo };
-
-            if (e.type === "device-permission-requested") {
-              response.allowManagerRequested = true;
-            } else if (e.type === "device-permission-granted") {
-              response.allowManagerGranted = true;
-            } else if (e.type === "result") {
-              response.listAppsRes = e.result;
+        return from(getDeviceInfo(transport)).pipe(
+          concatMap(deviceInfo => {
+            if (deviceInfo.isBootloader || deviceInfo.isOSU) {
+              return of({ ...seed, deviceInfo });
             }
 
-            return response;
-          }, seed),
+            // Preload things in parallel
+            manager.getLatestFirmwareForDevice(deviceInfo).catch(e => {
+              logger.warn(e);
+            });
+
+            return listApps(transport, deviceInfo).pipe(
+              startWith({ type: "loading" }),
+              scan((state: ManagerConnectState, e: *) => {
+                const response = { ...state, isLoading: true, deviceInfo };
+
+                if (e.type === "device-permission-requested") {
+                  response.allowManagerRequested = true;
+                } else if (e.type === "device-permission-granted") {
+                  response.allowManagerGranted = true;
+                } else if (e.type === "result") {
+                  response.listAppsRes = e.result;
+                }
+
+                return response;
+              }, seed),
+            );
+          }),
         );
       }),
     ),
--- src/renderer/screens/manager/index.js
+++ src/renderer/screens/manager/index.js
@@ -4,32 +4,62 @@ import { createStructuredSelector } from "reselect";
 import { getCurrentDevice } from "~/renderer/reducers/devices";
 import { connect } from "react-redux";
 import { command } from "~/renderer/commands";
-import Spinner from "~/renderer/components/Spinner";
+import Animation from "~/renderer/animations";
+import styled from "styled-components";
+import BigSpinner from "~/renderer/components/BigSpinner";
 import Dashboard from "~/renderer/screens/manager/Dashboard";
 import type { Device } from "~/renderer/reducers/devices";
 import type { ManagerConnectState } from "~/internal/commands/connectManager";
+import { Trans } from "react-i18next";
+import Text from "~/renderer/components/Text";
 
 type Props = {
   device?: Device,
 };
+
+const AnimationWrapper = styled.div`
+  width: 600px;
+  height: 350px;
+  align-self: center;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const Manager = ({ device }: Props) => {
   const initialState: ManagerConnectState = {
     device,
     allowManagerRequested: false,
     allowManagerGranted: false,
+    deviceInDashboard: true,
+    isLoading: false,
   };
 
   const [connectResult, setConnectResult] = useState(initialState);
   const [error, setError] = useState(null);
-  const { allowManagerRequested, allowManagerGranted, listAppsRes, deviceInfo } = connectResult;
+  const {
+    allowManagerRequested,
+    allowManagerGranted,
+    deviceInDashboard,
+    listAppsRes,
+    deviceInfo,
+  } = connectResult;
 
   useEffect(() => {
     if (device) {
       setError(null);
       const sub = command("connectManager")({ devicePath: device.path }).subscribe({
-        next: (next: ManagerConnectState) => setConnectResult(next),
-        error: error => setError(error),
+        next: (next: ManagerConnectState) => {
+          setConnectResult(next);
+        },
+        error: error => {
+          setError(error);
+        },
       });
 
       return () => sub.unsubscribe();
@@ -39,32 +69,49 @@ const Manager = ({ device }: Props) => {
     }
   }, [device]);
 
+  // console.log({
+  //   device: !!device,
+  //   deviceInfo: !!deviceInfo,
+  //   isLoading,
+  //   deviceInDashboard,
+  //   allowManagerRequested,
+  //   allowManagerGranted,
+  // });
+
   return (
     <Fragment>
-      {device ? (
-        error ? (
-          <div>
-            <h2>Error</h2>
-            <span>{error.message}</span>
-          </div>
-        ) : listAppsRes && deviceInfo ? (
-          <div>
-            <Dashboard device={device} deviceInfo={deviceInfo} listAppsRes={listAppsRes} />
-          </div>
-        ) : allowManagerRequested ? (
-          allowManagerGranted ? (
-            <h2>Manager permission granted</h2>
-          ) : (
-            <h2>Manager permission requested</h2>
-          )
-        ) : (
-          <div>
-            <h2>Loading</h2>
-            <Spinner size={60} />
-          </div>
-        )
+      {!device ? (
+        <Wrapper>
+          <AnimationWrapper>
+            <Animation name="connectDeviceEnterPinNanoX" />
+          </AnimationWrapper>
+          <Text ff="Inter|Regular" color="palette.text.shade100" align="center" fontSize={5}>
+            <Trans i18nKey="manager.connect.connectAndUnlockDevice" />
+          </Text>
+        </Wrapper>
+      ) : error ? (
+        <Wrapper>{error.message}</Wrapper>
+      ) : !deviceInDashboard ? (
+        <Wrapper>Device is not in dashboard</Wrapper>
+      ) : listAppsRes && deviceInfo ? (
+        <Dashboard device={device} deviceInfo={deviceInfo} listAppsRes={listAppsRes} />
+      ) : allowManagerRequested && !allowManagerGranted ? (
+        <Wrapper>
+          <h2>Manager permission requested</h2>
+        </Wrapper>
       ) : (
-        <h2>Connect your device</h2>
+        <Wrapper>
+          <BigSpinner size={50} />
+          <Text
+            mt={60}
+            ff="Inter|Regular"
+            color="palette.text.shade100"
+            align="center"
+            fontSize={5}
+          >
+            <Trans i18nKey="manager.connect.loading" />
+          </Text>
+        </Wrapper>
       )}
     </Fragment>
   );
