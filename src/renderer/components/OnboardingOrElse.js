4bf7ed3b11094237f267e3e733c594154378fe85
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-01-10 12:59:10 +0100

    skip onboarding when restarting app for now

--- src/renderer/components/OnboardingOrElse.js
+++ src/renderer/components/OnboardingOrElse.js
@@ -1,22 +1,23 @@
 // @flow
 
-import React, { memo } from "react";
-import { useSelector } from "react-redux";
-import { hasCompletedOnboardingSelector } from "~/renderer/reducers/settings";
-import { onboardingRelaunchedSelector } from "~/renderer/reducers/onboarding";
-import Onboarding from "~/renderer/screens/onboarding";
+import { memo } from "react";
+// import React, { memo } from "react";
+// import { useSelector } from "react-redux";
+// import { hasCompletedOnboardingSelector } from "~/renderer/reducers/settings";
+// import { onboardingRelaunchedSelector } from "~/renderer/reducers/onboarding";
+// import Onboarding from "~/renderer/screens/onboarding";
 
 type Props = {
   children: React$Node,
 };
 
 const OnboardingOrElseC = ({ children }: Props) => {
-  const hasCompletedOnboarding = useSelector(hasCompletedOnboardingSelector);
-  const onboardingRelaunched = useSelector(onboardingRelaunchedSelector);
+  // const hasCompletedOnboarding = useSelector(hasCompletedOnboardingSelector);
+  // const onboardingRelaunched = useSelector(onboardingRelaunchedSelector);
 
-  if (!hasCompletedOnboarding || onboardingRelaunched) {
-    return <Onboarding />;
-  }
+  // if (!hasCompletedOnboarding || onboardingRelaunched) {
+  //   return <Onboarding />;
+  // }
 
   return children;
 };
